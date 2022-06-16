b9a43163d506b579e21842311a18a3a953a373d0
eing7923 <107599566+eing7923@users.noreply.github.com>
2022-06-16 10:58:05 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1655351496,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/car-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/tree-sheet1.png",
+		"images/sta-sheet0.png",
+		"images/tnt-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
